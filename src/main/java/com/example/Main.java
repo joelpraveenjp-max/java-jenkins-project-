5bec5f31edd55990a54eda717c78242e6da83521
joelpraveenjp-max <joelpraveenjp@gmail.com>
2026-04-14 00:14:42 +0530

    Update Main.java

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,7 +1,20 @@
 package com.example;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Java Jenkins Pipeline Success!");
+        SpringApplication.run(Main.class, args);
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "Java is Running";
     }
 }
